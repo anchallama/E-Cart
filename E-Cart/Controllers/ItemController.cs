ddb3e6f1d928d64e846f65b307ebdc2365f8a8d1
Anchal lama <anchallama079@gmail.com>
2021-06-28 18:07:06 +0545

    Add project files.

---
+++ E-Cart/Controllers/ItemController.cs
@@ -0,0 +1,43 @@
+﻿using E_Cart.Models;
+using E_Cart.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Cart.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ECartDB3Entities objECartDB3Entities;
+        public ItemController()
+        {
+            objECartDB3Entities = new ECartDB3Entities();
+
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objECartDB3Entities.Categories
+                                                       select new SelectListItem()
+                                                       {
+                                                           Text = objCat.CategoryName,
+                                                           Value = objCat.CategoryId.ToString(),
+                                                           Selected = true
+
+                                                       });
+
+            return View(objItemViewModel);
+        }
+
+        [HttpPost]
+        public JsonResult Index(ItemViewModel objItemViewModel)
+        {
+
+            return Json("HHHH", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
